1a6e12aa6b11a234882e2a8aa1440f991f2728ca
Ilya Dimov <hawkeoni@zeliboba.vla.yp-c.yandex.net>
2022-06-03 14:42:57 +0300

    Add config & model init

--- real.sh
+++ real.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node 4 topformer/train.py --batch-size 32 --image-folder ../images/train/ --df-path ../images/train.csv 
+CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node 4 topformer/train.py --batch-size 32 --image-folder ../images/train/ --df-path ../images/train.csv  
--- topformer/train.py
+++ topformer/train.py
@@ -4,6 +4,7 @@ from time import time
 from argparse import ArgumentParser
 from pathlib import Path
 
+from PIL import Image
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -17,8 +18,9 @@ from scheduler import get_optimizer_and_scheduler
 from net import Topformer, SimpleHead, TopformerSegmenter
 from data import AlignedDataset
 from saveload import save_ckpt
-from common_segmentation import calculate_iou
+from common_segmentation import calculate_iou, get_palette
 
+PALETTE = get_palette(4)
 
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -45,8 +47,10 @@ def setup():
 def main(args):
     if dist.get_rank() == 0:
         print(args)
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
     MAX_ITERS = args.max_iters
-    cfg = Config.fromfile("topformer/config.py")
+    cfg = Config.fromfile(args.config)
     b_cfg = cfg.model.backbone
     backbone = Topformer(
         cfgs=b_cfg.cfgs, 
@@ -60,6 +64,14 @@ def main(args):
         init_cfg=b_cfg.init_cfg,
         num_heads=b_cfg.num_heads,
         )
+    if args.init_from:
+        state_dict = torch.load(args.init_from)["state_dict"]
+        state_dict = {k[len("backbone."):]: v for k, v in state_dict.items()}
+        nt = backbone.load_state_dict(state_dict, strict=False)
+        if rank == 0:
+            print(nt)
+
+        del state_dict
     h_cfg = cfg.model.decode_head
 
     head = SimpleHead(
@@ -69,8 +81,6 @@ def main(args):
         in_index=h_cfg.in_index,
         dropout_ratio=h_cfg.dropout_ratio,
     )
-    rank = dist.get_rank()
-    world_size = dist.get_world_size()
     model = TopformerSegmenter(backbone, head).to(rank)
     model = DDP(model, device_ids=[rank])
     optimizer, scheduler = get_optimizer_and_scheduler(
@@ -94,7 +104,7 @@ def main(args):
     )
     classes = ["background", "upper", "lower", "whole"]
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(weight=torch.Tensor([1., 1.5, 1.5, 1.5]).to(rank))
     
     start_time = time()
     if rank == 0:
@@ -103,12 +113,13 @@ def main(args):
         import random
         writer = SummaryWriter(flush_secs=10, filename_suffix="".join(random.choices(ascii_lowercase, k=10)))
     optimizer.zero_grad()
-    image, label = next(get_batch(dataloader))
-    if rank == 0:
-        import numpy as np
-        np.save("image.np", image.cpu().numpy())
-        np.save("label.np", label.cpu().numpy())
-    for iteration in range(1000):
+    # image, label = next(get_batch(dataloader))
+    # if rank == 0:
+    #     import numpy as np
+    #     np.save("image.np", image.cpu().numpy())
+    #     np.save("label.np", label.cpu().numpy())
+    # for iteration in range(1000):
+    for iteration, (image, label) in enumerate(get_batch(dataloader), start=1):
         image = image.to(rank)
         label = label.to(rank)
         prediction = model(image)
@@ -159,6 +170,8 @@ if __name__ == "__main__":
     parser.add_argument("--mean", type=float, default=0.5)
     parser.add_argument("--std", type=float, default=0.5)
     parser.add_argument("--lr", type=float, default=0.0003)
+    parser.add_argument("--config", type=str, default="topformer/config.py")
+    parser.add_argument("--init-from", type=str, default=None)
     args = parser.parse_args()
     setup()
     main(args)
