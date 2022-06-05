02c62b5c8726261ce5ac088c5af42b8bf1606d2f
Ilya Dimov <hawkeoni@zeliboba.vla.yp-c.yandex.net>
2022-06-05 18:32:45 +0300

    Remove junk bash scripts

--- rt.sh
+++
@@ -1,1 +0,0 @@
-torchrun --nproc_per_node 4 topformer/train.py --batch-size 32 --image-folder ../images/train/ --df-path ../images/train.csv --lr 0.003 --checkpoint-interval 20 --max-iters 400 --warmup-iters 50
